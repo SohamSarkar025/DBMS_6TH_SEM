d22bd376cd4ef78b2360bac003870e1a3ce53315
Soham Sarkar <srutidhara153@gmail.com>
2025-04-22 21:21:22 +0530

    1st

--- testfile.sql
+++ testfile.sql
@@ -1,29 +1,41 @@
+set serverout on
 
-set serveroutput on
-
-declare
-
-cursor cur is
-select empno,sal from emp where deptno=(select deptno from dept where dname='RESEARCH');
-emp_id number(10);
-emp_sal emp.sal%type;
-
+create or replace function prime_test2(id number) return number is
+num number(10);
 begin
-
-open cur;
-if cur%isopen then
-loop
-fetch cur into emp_id,emp_sal;
-exit when cur%notfound;
-update emp set sal=sal*0.02 where empno=emp_id;
-select sal into emp_sal from emp where empno=emp_id;
-insert into emp_sal_inc values(emp_id,emp_sal,sysdate);
-end loop;
-dbms_output.put_line(cur%rowcount);
-else
-dbms_output.put_line('cursor not found');
-end if;
-close cur;
+select num_id into num from prime_entry where prime_num=id;
+return 0;
+exception
+when no_data_found then
+return 1;
 end;
 /
-
+declare
+num number(10);
+n number;
+x number;
+flag number;
+i number;
+begin
+num:=&num;
+n:=TRUNC(num/2);
+for i in 2..n loop
+if(mod(num,2)=0)then
+flag:=1;
+exit;
+else
+flag:=0;
+end if;
+end loop;
+if(flag=1)then
+dbms_output.put_line('Number is not prime');
+else
+x:=prime_test2(num);
+if(x=1)then
+insert into prime_entry values(seq.nextval,num);
+else
+dbms_output.put_line('Number already exist..');
+end if;
+end if;
+end;
+/
